e4c666258952deba68ac89379b94a190fca07f17
Marta233 <martaassefa23@gmail.com>
2024-04-25 14:47:34 +0300

    streamlit dashboard

--- Home.py
+++ Home.py
@@ -1,14 +1,10 @@
 import pandas as pd
+import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
-import streamlit as st
-from src import eda_utils
-st.set_page_config(page_title="Sales Dashboard",
-                   page_icon=":bar_chart:",
-                   layout="wide"
-                   )
-st.title("Sales Dashboard")
+import altair as alt
+
+# Function to load data
 @st.cache_data
 def load_data(path: str):
     try:
@@ -24,7 +20,7 @@ def load_data(path: str):
             return pd.read_excel(path, engine='openpyxl')
         elif file_type == 'pdf':
             # For PDF files (if supported)
-            st.warning("PDF file format is not supported for direct loading.")
+            st.error("PDF file format is not supported for direct loading.")
             return None
         else:
             st.error("Unsupported file format. Please upload a CSV, XLSX, or PDF file.")
@@ -33,16 +29,221 @@ def load_data(path: str):
         st.error(f"An error occurred while loading the data: {str(e)}")
         return None
 
-with st.sidebar:
-    upload_file = st.file_uploader("Choose a file", type=["csv", "xlsx",'pdf'])
+# Function to calculate summary statistics
+def calculate_summary_stats(data, selected_column):
+    """Calculate summary statistics for the selected column."""
+    if selected_column == "Select a column":
+        return
+    
+    if selected_column in data.columns:
+        column_data = data[selected_column]
+        
+        if pd.api.types.is_numeric_dtype(column_data):
+            # For numeric columns
+            summary_stats = column_data.describe()
+            
+            # Calculate median, standard deviation, skewness, and kurtosis
+            median = column_data.median()
+            std = column_data.std()
+            skew = column_data.skew()
+            kurtosis = column_data.kurt()
+            
+            stats_data = {
+                "Statistic": ["Count", "Mean", "Median", "Standard Deviation", "Skewness", "Kurtosis"],
+                "Value": [summary_stats['count'], summary_stats['mean'], median, std, skew, kurtosis]
+            }
+            stats_df = pd.DataFrame(stats_data)
+            st.write(stats_df)
+        
+        else:
+            st.error("Unsupported column type. Please select a numerical column.")
+    
+    else:
+        st.error(f"Column '{selected_column}' not found in the data.")
 
-    if upload_file is None:
-        st.info("upload file through config",icon="ℹ️")
-        st.stop()
+# Function to count negative values
+def count_negative_values(data):
+    """Count negative values for each column."""
+    # Convert non-numeric values to NaN
+    data_numeric = data.apply(pd.to_numeric, errors='coerce')
+    # Count negative values for each column
+    negative_counts = (data_numeric < 0).sum()
+    return negative_counts
 
-Weather_Data = load_data(upload_file) 
+# Function to count missing values
+def missing_values(data):
+    """Count missing values for each column."""
+    missing_values = data.isnull().sum()
+    missing_percentage = (missing_values / len(data)) * 100
+    missing_data = pd.DataFrame({'Missing Count': missing_values, 'Missing Percentage': missing_percentage})
+    return missing_data
 
-col1, col2, col3 = st.columns([1,1,1])
-with col1.expander("Weather_Data"):
-    st.write(Weather_Data)
+# Function to plot time series
+# Function to plot time series
+def plot_time_series(cleaned_df):
+    """Plot time series for specified columns."""
+    # Check if 'Timestamp' column is present in the DataFrame
+    if 'Timestamp' not in cleaned_df.columns:
+        st.error("Error: 'Timestamp' column not found in the DataFrame.")
+        return
+    
+    # Define columns to plot
+    columns_to_plot = ['GHI', 'DNI', 'DHI']
+    
+    # Check if the columns exist in the DataFrame
+    for col in columns_to_plot:
+        if col not in cleaned_df.columns:
+            st.error(f"Error: Column '{col}' not found in the DataFrame.")
+            return
+    
+    # Convert 'Timestamp' column to datetime format
+    cleaned_df['Timestamp'] = pd.to_datetime(cleaned_df['Timestamp'])
 
+    # Set 'Timestamp' column as index
+    cleaned_df.set_index('Timestamp', inplace=True)
+
+    # Define colors
+    colors = sns.color_palette("husl", len(columns_to_plot))
+
+    # Plot time series
+    fig, ax = plt.subplots(figsize=(12, 6))
+    for i, col in enumerate(columns_to_plot):  # Specify columns to plot
+        sns.lineplot(data=cleaned_df, x=cleaned_df.index, y=col, label=col, color=colors[i], linewidth=2, alpha=0.8, ax=ax)
+    ax.set_xlabel('Time', fontsize=12)
+    ax.set_ylabel('Value', fontsize=12)
+    ax.set_title('Time Series Analysis for GHI, DNI, DHI', fontsize=16)
+    ax.legend(fontsize=10)
+    ax.grid(True, linestyle='--', alpha=0.5)
+    ax.tick_params(axis='both', labelsize=10)
+    plt.tight_layout()
+    
+    # Display the plot
+    st.pyplot(fig)
+# Function to plot time series for 'Tamb' column
+# Function to plot time series for 'Tamb' column
+def plot_time_series_tamb(cleaned_df):
+    """Plot time series for 'Tamb' column."""
+    # Check if 'Tamb' column is present in the DataFrame
+    if 'Tamb' not in cleaned_df.columns:
+        st.error("Error: 'Tamb' column not found in the DataFrame.")
+        return
+    
+    if 'Timestamp' not in cleaned_df.columns:
+        # If 'Timestamp' column is not present, use index as x-axis
+        x_axis = cleaned_df.index
+    else:
+        # Convert 'Timestamp' column to datetime format
+        cleaned_df['Timestamp'] = pd.to_datetime(cleaned_df['Timestamp'])
+        x_axis = 'Timestamp'
+
+    # Plot time series
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.lineplot(data=cleaned_df, x=x_axis, y='Tamb', ax=ax)
+    ax.set_xlabel('Time', fontsize=12)
+    ax.set_ylabel('Tamb', fontsize=12)
+    ax.set_title('Time Series Analysis for Tamb', fontsize=16)
+    ax.grid(True, linestyle='--', alpha=0.5)
+    ax.tick_params(axis='both', labelsize=10)
+    plt.tight_layout()
+    
+    # Display the plot
+    st.pyplot(fig)
+# Function for correlation analysis
+def correlation_analysis(data):
+    
+    # Select relevant columns for correlation analysis
+    relevant_columns = ['GHI', 'DNI', 'DHI', 'TModA', 'TModB']
+    relevant_data = data[relevant_columns]
+
+    # Calculate correlation matrix
+    correlation_matrix = relevant_data.corr()
+
+    # Plot heatmap
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5, ax=ax)
+    ax.set_title('Correlation Heatmap')
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
+    ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
+    st.pyplot(fig)
+
+# Function to plot boxplot with outliers for solar radiation and temperature data
+def plot_boxplot_outliers(data):
+
+    # Select relevant columns
+    solar_columns = ['GHI', 'DNI', 'DHI']
+    temp_columns = ['Tamb', 'TModA', 'TModB']
+
+    # Create subplots for solar radiation and temperature
+    fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(10, 8))
+
+    # Plot box plots for solar radiation
+    sns.boxplot(data=data[solar_columns], ax=axes[0])
+    axes[0].set_title('Solar Radiation (W/m²)')
+    axes[0].set_ylabel('Radiation (W/m²)')
+
+    # Plot box plots for temperature
+    sns.boxplot(data=data[temp_columns], ax=axes[1])
+    axes[1].set_title('Temperature (°C)')
+    axes[1].set_ylabel('Temperature (°C)')
+
+    # Adjust layout
+    plt.tight_layout()
+    st.pyplot(fig)
+
+
+# Main function
+def main():
+    st.title("Solar Pandel Installation Analysis")
+    with st.sidebar:
+        upload_file = st.file_uploader("Choose a file", type=["csv", "xlsx",'pdf'])
+
+        if upload_file is None:
+            st.info("Upload a file through the sidebar", icon="ℹ️")
+            st.stop()
+
+    Weather_Data = load_data(upload_file)
+
+    if Weather_Data is not None:
+        # Remove Timestamp column from the list of columns
+        columns_without_timestamp = [col for col in Weather_Data.columns if col != 'Timestamp']
+
+        col1, col2, col3 = st.columns([1, 1, 1])
+        with col1.expander("Weather Data"):
+            st.write(Weather_Data)
+
+        # Basic Information
+        with col2.expander("Description"):
+            st.write(Weather_Data.describe())
+
+        # Summary Statistics
+        with col3.expander("Summary Statistics"):
+            selected_column = st.selectbox("Select a column for statistical analysis", columns_without_timestamp, index=0)
+            calculate_summary_stats(Weather_Data, selected_column)
+
+        # Display the count of negative values
+        with col1.expander("Negative Value Counts:"):
+            st.write(count_negative_values(Weather_Data))
+        with col2.expander("Missing Values count"):
+            st.write(missing_values(Weather_Data))
+            
+        st.write("---")
+
+    col4, col5 = st.columns(2)
+    with col4:
+        st.subheader("Time Series Plot")
+        plot_time_series(Weather_Data)
+
+    with col5:
+        st.subheader("Time Series Plot for Tamb")
+        plot_time_series_tamb(Weather_Data)
+    col6,col7 = st.columns(2)
+    with col6:
+        st.subheader("Correlation between different variables:")
+        correlation_analysis(Weather_Data)
+    with col7:
+        st.subheader("outlier")
+        plot_boxplot_outliers(Weather_Data)
+
+
+if __name__ == "__main__":
+    main()
